35869707caea43fc1fd36d445cda68f53a1fe101
csemorf <csemorf@gmail.com>
2023-09-18 22:32:37 +0700

    1

--- components/modals/invite-modal.tsx
+++ components/modals/invite-modal.tsx
@@ -1,8 +1,9 @@
 "use client";
-import { DialogDescription } from "@radix-ui/react-dialog";
+
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog";
 
 import { useModal } from "@/hooks/use-modal-store";
+import { Label } from "../ui/label";
 
 const InviteModal = () => {
   const { isOpen, onClose, type } = useModal();
@@ -14,14 +15,12 @@ const InviteModal = () => {
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
         <DialogHeader className="pt-8 px-4">
           <DialogTitle className="font-bold text-center text-2xl">
-            Customize your server
+            Invite friends
           </DialogTitle>
-          <DialogDescription className="text-zinc-500">
-            Give your server a personality with a name a an image. You can
-            always change it later.
-          </DialogDescription>
         </DialogHeader>
-        Invite modal!
+        <div className="p-2">
+          <Label className="">Server invite link</Label>
+        </div>
       </DialogContent>
     </Dialog>
   );
