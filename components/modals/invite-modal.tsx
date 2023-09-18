202e4fc68eca257dcc40d533fa2cb0b6dea95b90
csemorf <csemorf@gmail.com>
2023-09-18 23:35:40 +0700

    11

--- components/modals/invite-modal.tsx
+++ components/modals/invite-modal.tsx
@@ -4,11 +4,30 @@ import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog";
 
 import { useModal } from "@/hooks/use-modal-store";
 import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { Check, Copy, RefreshCw } from "lucide-react";
+import { useOrigin } from "@/hooks/use-origin";
+import { useState } from "react";
+import { setTimeout } from "timers";
 
 const InviteModal = () => {
-  const { isOpen, onClose, type } = useModal();
+  const { isOpen, onClose, type, data } = useModal();
 
   const isModalOpen = isOpen && type === "invite";
+  const origin = useOrigin();
+  const { server } = data;
+  const inviteUrl = `${origin}/invite/${server?.inviteCode}`;
+
+  const [copied, setCopied] = useState(false);
+
+  const onCopy = () => {
+    navigator.clipboard.writeText(inviteUrl);
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+  };
 
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
@@ -19,7 +38,26 @@ const InviteModal = () => {
           </DialogTitle>
         </DialogHeader>
         <div className="p-2">
-          <Label className="">Server invite link</Label>
+          <Label className="text-xs font-bold uppercase text-zinc-500 dark:text-secondary/70">
+            Server invite link
+          </Label>
+          <div className="flex items-center gap-x-2 mt-2">
+            <Input
+              value={inviteUrl}
+              className="border-0 bg-zinc-300/50 text-black focus-visible:ring-0 focus-visible:ring-offset-0"
+            ></Input>
+            <Button size="icon" onClick={onCopy}>
+              {copied ? <Check></Check> : <Copy className="w-4 h-4 "></Copy>}
+            </Button>
+          </div>
+          <Button
+            size="sm"
+            variant="link"
+            className="text-xs  text-zinc-500 mt-4"
+          >
+            Generate a New Link
+            <RefreshCw size="" className="w-4 h-4 ml-2"></RefreshCw>
+          </Button>
         </div>
       </DialogContent>
     </Dialog>
